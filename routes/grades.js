16d7d20ead1d7f3f38bbeac60b367f38e75ac3da
pupboss <pupboss.i@gmail.com>
2016-03-12 22:56:29 +0800

    Refactored code.

--- routes/grades.js
+++ routes/grades.js
@@ -4,17 +4,14 @@
 
 var express = require('express');
 var router = express.Router();
-var agent = require('../agent/dom_agent');
 var parser = require('../agent/parser/grades');
 
 router.get('/:name', function (req, res) {
-  agent.normal_agent(req.lntu_user_id, req.lntu_password, 'student/queryscore/queryscore.jsdo', function (err, final) {
+  parser(req.lntu_user_id, req.lntu_password, 'student/queryscore/queryscore.jsdo', function (err, result) {
     if (err) {
       return res.status(400).json({ code: err, message: 'it seems something went wrong' });
     }
-    parser(req.lntu_user_id, final, function (result) {
-      return res.status(200).json(result);
-    });
+    return res.status(200).json(result);
   });
 });
 
--- routes/student.js
+++ routes/student.js
@@ -5,37 +5,33 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../utility/db');
-var agent = require('../agent/dom_agent');
 var parser = require('../agent/parser/student');
 
 router.get('/:name', function (req, res) {
 
-  agent.normal_agent(req.lntu_user_id, req.lntu_password, 'student/studentinfo/studentinfo.jsdo', function (err, final) {
-
+  parser(req.lntu_user_id, req.lntu_password, 'student/studentinfo/studentinfo.jsdo', function (err, result) {
     if (err) {
       return res.status(400).json({ code: err, message: 'it seems something went wrong' });
     }
-    parser(final, function (result) {
-      model.user_detail_model.find({ id: result['id'] }, function (error, docs) {
-        var user_detail = {
-          id: result['id'],
-          name: result['name'],
-          photo_url: result['photoUrl'],
-          class_info: result['classInfo'],
-          sex: result['sex'],
-          college: result['college'],
-          update_at: new Date().toISOString()
-        };
-        if(error || docs.length < 1){
-          model.user_detail_model.create(user_detail, function (error, docs) {
-          });
-        } else {
-          model.user_detail_model.update({ id: result['id'] }, user_detail, function (error, docs) {
-          });
-        }
-      });
-      return res.status(200).json(result);
+    model.user_detail_model.find({ id: result['id'] }, function (error, docs) {
+      var user_detail = {
+        id: result['id'],
+        name: result['name'],
+        photo_url: result['photoUrl'],
+        class_info: result['classInfo'],
+        sex: result['sex'],
+        college: result['college'],
+        update_at: new Date().toISOString()
+      };
+      if(error || docs.length < 1){
+        model.user_detail_model.create(user_detail, function (error, docs) {
+        });
+      } else {
+        model.user_detail_model.update({ id: result['id'] }, user_detail, function (error, docs) {
+        });
+      }
     });
+    return res.status(200).json(result);
   });
 });
 
