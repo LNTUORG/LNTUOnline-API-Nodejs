44325a84fde7a19a8cfea9e8eaed9919ba501928
pupboss <pupboss.i@gmail.com>
2016-03-13 00:56:49 +0800

    Skill test info finished.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var student = require('./routes/student');
 var grades = require('./routes/grades');
 var unpass = require('./routes/unpass');
 var exam_plan = require('./routes/exam_plan');
-//var extra_score = require('./routes/extra_score');
+var extra_score = require('./routes/extra_score');
 //var class_table = require('./routes/class_table');
 
 app.use(body_parser.urlencoded({ extended: false }));
@@ -29,7 +29,7 @@ app.use('/student', student);
 app.use('/grades', grades);
 app.use('/unpass-course', unpass);
 app.use('/exam-plan', exam_plan);
-//app.use('/skill-test-score', extra_score);
+app.use('/skill-test-score', extra_score);
 //app.use('/class-table', class_table);
 
 var server = app.listen(config.port, function () {
--- routes/extra_score.js
+++ routes/extra_score.js
@@ -1,3 +1,18 @@
 /**
  * Created by pupboss on 3/12/16.
  */
+
+var express = require('express');
+var router = express.Router();
+var parser = require('../parser/extra_score');
+
+router.get('/:name', function (req, res) {
+  parser(req.lntu_user_id, req.lntu_password, 'student/queryscore/skilltestscore.jsdo', function (err, result) {
+    if (err) {
+      return res.status(400).json({ code: err, message: 'it seems something went wrong' });
+    }
+    return res.status(200).json(result);
+  });
+});
+
+module.exports = router;
