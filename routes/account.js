805a8c3245cf9c590c046bf7141caf2ee0f8673a
pupboss <pupboss.i@gmail.com>
2016-03-14 11:40:20 +0800

    Finished

--- routes/account.js
+++ routes/account.js
@@ -29,7 +29,7 @@ router.post('/login', function (req, res) {
     user.type = 'TEACHER'
   }
   res.contentType('application/json');
-  agent.just_get_cookie(req.body['userId'], req.body['password'], function (err) {
+  agent.get_cookie(req.body['userId'], req.body['password'], function (err) {
     if (err == constant.cookie.user_error) {
       return res.status(400).json({ code: err, message: 'password error' });
     } else if (err == constant.cookie.net_error) {
