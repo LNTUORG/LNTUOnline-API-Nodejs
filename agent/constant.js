f984a8a4bfdc3d29263f9f655de0af0818b811aa
pupboss <pupboss.i@gmail.com>
2016-06-17 18:00:50 +0800

    Updated URLS

--- agent/constant.js
+++ agent/constant.js
@@ -16,9 +16,9 @@ var constant = {
     'http://60.18.131.131:11180/academic/',    //*
     'http://60.18.131.131:11181/academic/',
     'http://60.18.131.131:11080/newacademic/', //*
-    'http://60.18.131.131:11081/newacademic/',
-    'http://60.18.131.133:11180/newacademic/', //*
-    'http://60.18.131.133:11181/newacademic/'
+    'http://60.18.131.131:11081/newacademic/'
+    // 'http://60.18.131.133:11180/newacademic/', //*
+    // 'http://60.18.131.133:11181/newacademic/'
   ]
 };
 
